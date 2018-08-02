f1f8866c35e3dbf9530819272a484c6c7f2f15e4
inasync <in-async@users.noreply.github.com>
2018-08-02 16:34:04 +0900

    MSTest 向けに Verify の拡張メソッドを追加。

--- TestCaseRunner.MSTest/TestCaseRunner.MSTest.cs
+++ TestCaseRunner.MSTest/TestCaseRunner.MSTest.cs
@@ -8,6 +8,7 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting {
         /// <summary>
         /// テスト対象コードの実行結果を検証します。
         /// </summary>
+        /// <param name="actual">テスト対象コードの実行結果。</param>
         /// <param name="resultVerifier">テスト対象コードの戻り値の検証を行うデリゲート。</param>
         /// <param name="expectedExceptionType">テスト対象コードによって生じる事が期待される例外の型。</param>
         /// <exception cref="ArgumentNullException"><paramref name="actual"/> or <paramref name="resultVerifier"/> is <c>null</c>.</exception>
@@ -20,6 +21,8 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting {
         /// <summary>
         /// テスト対象コードの実行結果を検証します。
         /// </summary>
+        /// <typeparam name="TResult">テスト対象コードの戻り値の型。</typeparam>
+        /// <param name="actual">テスト対象コードの実行結果。</param>
         /// <param name="resultVerifier">テスト対象コードの戻り値の検証を行うデリゲート。</param>
         /// <param name="expectedExceptionType">テスト対象コードによって生じる事が期待される例外の型。</param>
         /// <exception cref="ArgumentNullException"><paramref name="actual"/> or <paramref name="resultVerifier"/> is <c>null</c>.</exception>
@@ -27,6 +30,23 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting {
             if (actual == null) { throw new ArgumentNullException(nameof(actual)); }
 
             actual.Verify(resultVerifier, (exception, description) => Assert.AreEqual(expectedExceptionType, exception?.GetType(), description));
+        }
+
+        /// <summary>
+        /// テスト対象コードの実行結果を検証します。
+        /// </summary>
+        /// <typeparam name="TResult">テスト対象コードの戻り値の型。</typeparam>
+        /// <param name="actual">テスト対象コードの実行結果。</param>
+        /// <param name="expectedResult">テスト対象コードの戻り値として期待される値。</param>
+        /// <param name="expectedExceptionType">テスト対象コードによって生じる事が期待される例外の型。</param>
+        /// <exception cref="ArgumentNullException"><paramref name="actual"/> or <paramref name="resultVerifier"/> is <c>null</c>.</exception>
+        public static void Verify<TResult>(this ITestActual<TResult> actual, TResult expectedResult, Type expectedExceptionType) {
+            if (actual == null) { throw new ArgumentNullException(nameof(actual)); }
+
+            actual.Verify(
+                (result, description) => Assert.AreEqual(expectedResult, result, description),
+                (exception, description) => Assert.AreEqual(expectedExceptionType, exception?.GetType(), description)
+            );
         }
     }
 }
