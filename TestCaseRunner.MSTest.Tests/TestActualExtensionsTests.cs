2ca25a85d5446e8babd72aeca4e495121965f09f
inasync <in-async@users.noreply.github.com>
2018-10-01 16:36:59 +0900

    コレクションに対する Verify 拡張メソッドは十分に吟味できていないので一旦削除。

--- TestCaseRunner.MSTest.Tests/TestActualExtensionsTests.cs
+++ TestCaseRunner.MSTest.Tests/TestActualExtensionsTests.cs
@@ -115,10 +115,5 @@ namespace InAsync.Tests {
 
             ITestActual<TResult> TestActual<TResult>(TResult result, Exception ex) => new TestCaseRunner("desc").Run(() => (ex == null) ? result : throw ex);
         }
-
-        [TestMethod]
-        public void Verify_TResult_TResults_IComparer_Type() {
-            Assert.Fail();
-        }
     }
 }
--- TestCaseRunner.MSTest/TestActualExtensions.cs
+++ TestCaseRunner.MSTest/TestActualExtensions.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using InAsync;
 
 namespace Microsoft.VisualStudio.TestTools.UnitTesting {
@@ -61,27 +58,6 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting {
 
             actual.Verify(
                 (result, description) => Assert.AreEqual(expectedResult, result, description),
-                (exception, description) => Assert.AreEqual(expectedExceptionType, exception?.GetType(), description)
-            );
-        }
-
-        /// <summary>
-        /// テスト対象コードの実行結果を検証します。
-        /// </summary>
-        /// <typeparam name="TResult">テスト対象コードの戻り値の型。</typeparam>
-        /// <param name="actual">テスト対象コードの実行結果。</param>
-        /// <param name="expectedResult">テスト対象コードの戻り値として期待される値。</param>
-        /// <param name="resultComparer"><typeparamref name="TResult"/> の比較子。<c>null</c> の場合は <see cref="Comparer{T}.Default"/> が使用される。</param>
-        /// <param name="expectedExceptionType">テスト対象コードによって生じる事が期待される例外の型。</param>
-        /// <exception cref="ArgumentNullException"><paramref name="actual"/> or <paramref name="resultVerifier"/> is <c>null</c>.</exception>
-        public static void Verify<TResult>(this ITestActual<IEnumerable<TResult>> actual, IEnumerable<TResult> expectedResult, IComparer<TResult> resultComparer, Type expectedExceptionType) {
-            if (actual == null) { throw new ArgumentNullException(nameof(actual)); }
-            if (resultComparer == null) {
-                resultComparer = Comparer<TResult>.Default;
-            }
-
-            actual.Verify(
-                (result, description) => CollectionAssert.AreEqual(expectedResult.ToArray(), result.ToArray(), (IComparer)resultComparer, description),
                 (exception, description) => Assert.AreEqual(expectedExceptionType, exception?.GetType(), description)
             );
         }
